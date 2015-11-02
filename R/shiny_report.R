d63898dc27cd1152c1f940db07cc81b79c5f0983
Paul Harrison <pfh@logarithmic.net>
2015-11-03 09:26:51 +1100

    Add basic shiny report

--- R/shiny_report.R
+++ R/shiny_report.R
@@ -3,14 +3,14 @@
 # All of the plots.
 #
 
-shiny_report <- function(y, counts=NULL) {
+shiny_report <- function(y, counts=NULL, prefix="") {
     p <- function(name) paste0(prefix,name)
 
     y <- ensure_reactable(y)
     counts <- ensure_reactable(counts)
 
-    stability <- shiny_stability(y, counts, prefix="stability")
-    biplot <- shiny_biplot(y, prefix="biplot")
+    stability <- shiny_stability(y, counts, prefix=p("stability"))
+    biplot <- shiny_biplot(y, prefix=p("biplot"))
 
     ui <- shiny::navlistPanel(
         widths=c(2,10),
