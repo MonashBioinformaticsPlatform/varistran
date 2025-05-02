31f6e7d507069b51370be2dc08cf8f7910454763
Paul Harrison <pfh@logarithmic.net>
2025-05-02 11:13:06 +1000

    Fixes and documentation

--- R/samesum.R
+++ R/samesum.R
@@ -40,23 +40,25 @@ get_scale <- function(x, target, tol=1e-9, verbose=FALSE) {
 #'
 #' This is a method of computing log counts while dealing sensibly with zeros and differing library sizes. It should cope well with very sparse data. The input is transformed using \code{log2(x/scale+1)} with a different scale for each sample. The scale for each sample is chosen so all of the samples add up to the same value.
 #'
-#' You can either specify the argument \code{scale}, and it will try to give individual samples scale values close to this value, or you can directly specify the value samples should add up to with \code{target_sum}.
+#' You can either specify the argument \code{scale}, and it will try to give individual samples scale values close to this value, or directly specify the target sum with \code{sum}.
 #'
 #' \code{samesum_log2_cpm} adds a constant to the result to produce values that can be treated as log2 Counts Per Million (CPM). The result will have the property that \code{mean(colSums(2^lcpm-2^attr(lcpm,"zero")))} equals one million.
+#'
+#' Unlike using library size normalization, the samesum method is robust to variations in high abundance features, because it works on the log transformed values. It is also robust to noise from low abundance features because of the \code{+1} in the transformation.
 #'
 #' This method has some similarity to Centered Log Ratio (CLR) transformation. Where the CLR ensures each sample adds to zero, the samesum transformation ensures each sample adds to the same non-zero value. Like the CLR, distances between samesum transformed data are a good way to measure sample similarity. Transformed values could be used with PCA or with non-linear dimension reduction methods.
 #'
 #' Samesum transformed data can also be used in differential abundance analysis using the limma-trend or limma-vooma methods.
 #'
-#' The default value of 2 for scale should be reasonable for count data, but is not variance stabilizing. limma-trend or limma-vooma will take this into account. For visualization and exploratory methods you could try a larger scale value, which will damp down variation in low abundance rows. The scale parameter acts similarly to a pseudocount in log transformation methods with a pseudocount parameter such as edgeR's \code{cpm} function.
+#' The default value of 2 for scale should be reasonable for count data, but is not variance stabilizing. limma-trend or limma-vooma will take this into account. For visualization and exploratory methods you could try a larger scale value, which will damp down variation in low abundance rows and provide better variance stabilization. The scale parameter acts similarly to a pseudocount in log transformation methods with a pseudocount parameter (such as edgeR's \code{cpm} function).
 #'
 #' When different samples have different library sizes, some of them may be given a scale much smaller than the target scale. When this happens, these samples may appear as outliers in data visualization, purely as an artifact of the transformation. A warning is produced if any samples are given a scale less than one. Consider excluding these samples from your analysis. If all the samples need to be used, you can increase the target scale to avoid this problem. This will allow you to compare all samples, but at the cost of losing some ability to resolve fine differences between samples.
 #'
 #' @param x A matrix or a sparse matrix. Usually these will be counts, but any matrix of non-negative values could potentially be used. We regard columns as samples.
 #'
-#' @param scale Aim to give each sample close to this value of scale. The default value of 2 should be reasonable for count data, but see discussion below.
+#' @param scale Target scale. Aim to give each sample close to this value of scale. The default value of 2 should be reasonable for count data, but see discussion below.
 #'
-#' @param target_sum Aim to give each sample exactly this sum. \code{scale} is ignored if this parameter is used. This could be used to provide a transformation that is consistent across datasets.
+#' @param sum Target sum. Aim to give each sample exactly this sum. \code{scale} is ignored if this parameter is used. This could be used to provide a transformation that is consistent across datasets.
 #'
 #' @param tol Tolerance when optimizing scale values. Scale is increased using Newton's method until the sum is at least \code{target_sum*(1-tol)}.
 #'
@@ -74,19 +76,19 @@ get_scale <- function(x, target, tol=1e-9, verbose=FALSE) {
 #' }
 #'
 #' @export
-samesum_log2_norm <- function(x, scale=2, target_sum=NA, tol=1e-9, verbose=FALSE) {
+samesum_log2_norm <- function(x, scale=2, sum=NA, tol=1e-9, verbose=FALSE) {
     # Calculations are all done using natural log
     
-    if (is.na(target_sum)) {
+    if (is.na(sum)) {
         baseline <- colSums(log1p(x/scale))
-        target_sum <- mean(baseline)
+        sum <- mean(baseline)
     } else {
-        target_sum <- target_sum * log(2)
+        sum <- sum * log(2)
     }
     
     scales <- rep(0, ncol(x))
     for(i in seq_len(ncol(x))) {
-        scales[i] <- get_scale(x[,i], target=target_sum, tol=tol, verbose=verbose)
+        scales[i] <- get_scale(x[,i], target=sum, tol=tol, verbose=verbose)
     }
     
     # Comment on anything concerning
@@ -117,7 +119,8 @@ samesum_log2_norm <- function(x, scale=2, target_sum=NA, tol=1e-9, verbose=FALSE
     samples$scale <- scales
     
     scaled_sums <- sums / samples$scale
-    norm_factor <- scaled_sums/exp(mean(log(scaled_sums))) 
+    good <- is.finite(scaled_sums) & scaled_sums != 0
+    norm_factor <- scaled_sums/exp(mean(log(scaled_sums[good]))) 
     samples$true_lib_size <- sums
     samples$adjusted_lib_size <- sums * norm_factor 
     
@@ -126,19 +129,20 @@ samesum_log2_norm <- function(x, scale=2, target_sum=NA, tol=1e-9, verbose=FALSE
     
     attr(result,"samples") <- samples
     attr(result,"zero") <- 0
-    attr(result,"sum") <- target_sum
+    attr(result,"sum") <- sum / log(2)
     
     result
 }
 
 #' @rdname samesum_log2_norm
 #' @export
-samesum_log2_cpm <- function(x, scale=2, target_sum=NA, tol=1e-9, verbose=FALSE) {
-    result <- samesum_log2_norm(x, scale=scale, target_sum=target_sum, tol=tol, verbose=verbose)
+samesum_log2_cpm <- function(x, scale=2, sum=NA, tol=1e-9, verbose=FALSE) {
+    result <- samesum_log2_norm(x, scale=scale, sum=sum, tol=tol, verbose=verbose)
     
     samples <- attr(result,"samples")
-    scaled_sums <- samples$true_lib_size * samples$scale
-    adj <- log2(1e6) - log2(mean(scaled_sums)) 
+    scaled_sums <- samples$true_lib_size / samples$scale
+    good <- is.finite(scaled_sums) & scaled_sums != 0
+    adj <- log2(1e6) - log2(mean(scaled_sums[good]))
     
     result <- result + adj
     attr(result, "zero") <- adj
